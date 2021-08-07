67fb95a7b76296c88ffc81ba018e72393a99ccc0
Thet Htet Aung <thet@tractive.com.my>
2021-08-07 14:34:15 +0630

    Implement google signin with firebase

--- App.tsx
+++ App.tsx
@@ -1,12 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import { fetchUserInfoAsync } from 'expo-auth-session';
 
-export default function App() {
+WebBrowser.maybeCompleteAuthSession();
+
+const App = () => {
+  const [user, setUser] = useState<any>();
+  
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: '969557936447-b0p1hp6fds4rsarp9kce5t1acqvmc9t5.apps.googleusercontent.com',
+  });
+  const fetchUser = async (accessToken: string) => {
+    fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${accessToken}`)
+      .then(response => response.json())
+      .then(data => {
+        if(data) {
+          console.log(data);
+          setUser(data)
+        }
+      })
+  }
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+        const { authentication } = response;
+        console.log('Authentication:', authentication);
+        if(authentication?.accessToken) {
+          fetchUser(authentication.accessToken);
+        }
+      }
+  }, [response]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title="Sign In with Google"
+        onPress={() => promptAsync()}>
+      </Button>
+      {
+        user && <Text>{JSON.stringify(user)}</Text>
+      }
     </View>
   );
 }
@@ -19,3 +54,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,6 @@
+## How to implement firebase login
+
+clientId : 969557936447-b0p1hp6fds4rsarp9kce5t1acqvmc9t5.apps.googleusercontent.com
+clientSecret : xk6K7zHb78vlcVGGreYTf6F_
+
+- Create firebase project
